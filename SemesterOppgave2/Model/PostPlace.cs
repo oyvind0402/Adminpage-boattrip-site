1d34c2ce871c7596febeb80c8ecc34fc90495510
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2021-10-18 01:21:32 +0200

    Add project files.

---
+++ SemesterOppgave2/Model/PostPlace.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SemesterOppgave2.Model
+{
+    public class PostPlace
+    {
+    }
+}
