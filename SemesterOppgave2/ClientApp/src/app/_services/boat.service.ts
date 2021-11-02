32ad9fc5c9057280455c238f46732fae7bcb5aa7
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2021-11-02 15:16:24 +0100

    Fixed import in boat

--- SemesterOppgave2/ClientApp/src/app/_services/boat.service.ts
+++ SemesterOppgave2/ClientApp/src/app/_services/boat.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Boat } from '../models/Boat';
+import { Boat } from '../models/boat';
 
 
 @Injectable()
